7a2baf0137926d90a0d1a9dc6c51a72f71aa3e89
SEM <samerx777222@gmail.com>
2023-10-21 15:51:44 +0200

    Added P component for text.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import {Button, Htag} from "@/components";
+import {Button, Htag, P} from "@/components";
 
 export default function Home(): JSX.Element {
     return (<>
             <Htag tag='h1'>Text</Htag>
             <Button appearance='primary' arrow='right'>Button</Button>
             <Button appearance='ghost' arrow='down'>Button2</Button>
+            <P size={"s"}>S text</P>
+            <P size={"m"}>M text</P>
+            <P size={"l"}>L text</P>
         </>
     );
 }
